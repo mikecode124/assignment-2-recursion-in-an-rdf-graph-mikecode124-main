866e72f524fbd7aa1aaea391dd6558a03d4315f2
mikecode124 <michaelkimmail@gmail.com>
2025-10-23 17:12:49 -0700

    basic class structure created. Ask professor about public / private, ID / predicate storage

--- RDFOBject.h
+++ RDFOBject.h
@@ -7,6 +7,31 @@
 
 
 class RDFOBject {
+public:
+    // constructors
+    RDFObject();
+    RDFObject(const std:string& fileName);
+
+    // destructor
+    ~RDFObject();
+
+    // overloaded operators
+    friend std::istream& operator>>(std::istream& in, RDFObject& toRead);
+    friend std::ostream& operator<<(std::ostream& out, RDFObject& toWrite);
+
+    // print all rdfobjects in nodes to file (gml format)
+    void GraphPrint(const std:string& fileOut);
+
+
+private:
+    std::string id; // unique identifier
+    std::vector<std::pair<std::string, std::string>> predicates; // predicates stores <role, object> pairs
+
+    /*
+     *  nodes <unique id, RDFobject>: (Keys: string id, Value: RDFObject pointer)
+     *  a static (non-member) hashmap which contains all the RDFObjects created from file.
+     */
+    static std::unordered_map<std::string, RDFObject*> nodes;
 
 };
 
